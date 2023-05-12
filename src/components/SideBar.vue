879045b3d230b5814229d4084886c8917816fc2c
rdyjun <rdyjun00@gmail.com>
2023-05-12 20:58:42 +0900

    feature: add home page to sidebar component

---
+++ src/components/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="SideBar">
+    <nav>
+      <ul>
+        <li v-for="item in menu" :key="item">{{ item }}</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideBar',
+  data () {
+    return {
+      menu: ['상품관리', 'ㅁ']
+    }
+  }
+}
+</script>
+
+<style>
+  @import '@/style/sidebar.css';
+  @import '@/style/wrap.css';
+</style>
