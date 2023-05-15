9ec6eaa8f51ce5b6418ed368df05a73414e240a2
rdyjun <rdyjun00@gmail.com>
2023-05-15 14:57:51 +0900

    feat: add header and logo

---
+++ src/components/HeaderLine.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="HeaderLine">
+      <a><img v-bind:src="logoSrc"></a>
+    </div>
+  </template>
+
+<script>
+import logoSrc from '@/assets/logo.png'
+
+export default {
+  name: 'HeaderLine',
+  data () {
+    return {
+      logoSrc: logoSrc
+    }
+  }
+}
+</script>
+
+<style>
+  @import '@/style/header.css';
+  @import '@/style/wrap.css';
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <HeaderLine />
     <SideBar />
   </div>
 </template>
@@ -7,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import SideBar from '@/components/SideBar.vue'
+import HeaderLine from '@/components/HeaderLine.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    SideBar
+    SideBar,
+    HeaderLine
   }
 }
 
