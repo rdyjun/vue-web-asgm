8b4bcfc9c91543f0495f6160239e08cb1175a17c
rdyjun <rdyjun00@gmail.com>
2023-06-13 16:47:51 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
+  outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
+    ? '/my-project/'  // 'my-project'를 실제 GitHub 리포지토리 이름으로 변경하세요.
     : '/'
 }
